6bc1adc2636cb15ec7df2d6e8c2f0140917a0d30
yaluvv <yaluvv@yandex.ru>
2023-06-07 21:45:54 +0300

    fix src bug could not resolve

--- src/components/Layout/Header/menu/Menu.jsx
+++ src/components/Layout/Header/menu/Menu.jsx
@@ -1,7 +1,7 @@
 import styles from "./Menu.module.scss";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
-import UserMenu from "../user-menu/userMenu";
+import UserMenu from "../user-menu/UserMenu";
 import React from "react";
 
 const Menu = () => {
